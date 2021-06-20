d6f8c418bd4b5644442995c9ea1a5fea12747e85
Judit Turcsanyi <judit.turcsanyi@hotmail.com>
2021-06-20 23:28:48 +0200

    June update

--- src/pages/Milestones.js
+++ src/pages/Milestones.js
@@ -1,106 +1,144 @@
-import React from 'react';
-import styled from 'styled-components';
-import Timeline from '../components/Timeline';
-import {motion} from 'framer-motion';
-import {componentsAnimation, component} from '../animations'
+import React from "react"
+import styled from "styled-components"
+import Timeline from "../components/Timeline"
+import { motion } from "framer-motion"
+import { componentsAnimation, component } from "../animations"
 
 const Milestones = () => {
-    return(
-        <Container variants={componentsAnimation} initial="hidden" animate="show" exit="exit">
-            <motion.div variants={component} className="milestones">
-                <h3>My Road to Become a Frontend Developer</h3>
-                <Timeline />
-            </motion.div>
-            <Goals variants={component}>
-                <div className="text">
-                    <h3>These are my current skills and biggest targets for the near future*:</h3>
-                    <i>*Subject to change may an amazing opportunity arise ;)</i>
-                    <p>The Basics</p>
-                    <input type="checkbox" className="checkbox" name="html" checked readOnly/>
-                    <label htmlFor="html"> HTML</label><br/>
-                    <input type="checkbox" name="css" checked readOnly/>
-                    <label htmlFor="css"> CSS</label><br/>
-                    <input type="checkbox" name="sass" checked readOnly/>
-                    <label htmlFor="sass"> SASS</label><br/>
-                    <input type="checkbox" name="js" checked readOnly/>
-                    <label htmlFor="js"> JavaScript</label><br/>
-                    <input type="checkbox" name="api" checked readOnly/>
-                    <label htmlFor="api"> APIs</label><br/>
-                    <input type="checkbox" name="harvard" checked="" readOnly/>
-                    <label htmlFor="harvard"> Harvard CS50</label><br/>
-                    <p>JavaScript Libraries, Frameworks</p>
-                    <input type="checkbox" name="react" checked readOnly/>
-                    <label htmlFor="react"> ReactJS</label><br/>
-                    <input type="checkbox" name="reactn" checked="" readOnly/>
-                    <label htmlFor="reactn"> React Native</label><br/>
-                    <input type="checkbox" name="redux" checked readOnly/>
-                    <label htmlFor="redux"> Redux</label><br/>
-                    <input type="checkbox" name="angular" checked="" readOnly/>
-                    <label htmlFor="angular"> Angular</label><br/>
-                    <input type="checkbox" name="gatsby" checked="" readOnly/>
-                    <label htmlFor="gatsby"> Gatsby</label><br/>
-                    <input type="checkbox" name="gulp" checked="" readOnly/>
-                    <label htmlFor="gulp"> Gulp</label><br/>
-                    <p>Version Control</p>
-                    <input type="checkbox" name="git" checked readOnly/>
-                    <label htmlFor="git"> Git/GitHub</label><br/>
-                    <p>Test Framework</p>
-                    <input type="checkbox" name="mocha" checked="" readOnly/>
-                    <label htmlFor="mocha"> Mocha/Chai</label><br/>
-                    <p>Database Programs</p>
-                    <input type="checkbox" name="sql" checked="" readOnly/>
-                    <label htmlFor="sql"> SQL</label><br/>
-                    <input type="checkbox" name="mongo" checked="" readOnly/>
-                    <label htmlFor="mongo"> MongoDB</label><br/>
-                    <p>Cloud Computing Platforms</p>
-                    <input type="checkbox" name="aws" checked="" readOnly/>
-                    <label htmlFor="aws"> AWS</label><br/>
-                </div>
-            </Goals>
-        </Container>
-    )
+  return (
+    <Container
+      variants={componentsAnimation}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+    >
+      <motion.div variants={component} className="milestones">
+        <h3>My Road to Become a Frontend Developer</h3>
+        <Timeline />
+      </motion.div>
+      <Goals variants={component}>
+        <div className="text">
+          <h3>
+            These are my current skills and biggest targets for the near
+            future*:
+          </h3>
+          <i>*Subject to change may an amazing opportunity arise ;)</i>
+          <p>The Basics</p>
+          <input
+            type="checkbox"
+            className="checkbox"
+            name="html"
+            checked
+            readOnly
+          />
+          <label htmlFor="html"> HTML</label>
+          <br />
+          <input type="checkbox" name="css" checked readOnly />
+          <label htmlFor="css"> CSS</label>
+          <br />
+          <input type="checkbox" name="sass" checked readOnly />
+          <label htmlFor="sass"> SASS</label>
+          <br />
+          <input type="checkbox" name="js" checked readOnly />
+          <label htmlFor="js"> JavaScript</label>
+          <br />
+          <input type="checkbox" name="ts" checked="" readOnly />
+          <label htmlFor="ts"> TypeScript</label>
+          <br />
+          <input type="checkbox" name="api" checked readOnly />
+          <label htmlFor="api"> APIs</label>
+          <br />
+          <input type="checkbox" name="harvard" checked="" readOnly />
+          <label htmlFor="harvard">
+            {" "}
+            Harvard CS50x - Introduction to Computer Science
+          </label>
+          <br />
+          <p>JavaScript Libraries, Frameworks</p>
+          <input type="checkbox" name="react" checked readOnly />
+          <label htmlFor="react"> ReactJS</label>
+          <br />
+          <input type="checkbox" name="reactn" checked="" readOnly />
+          <label htmlFor="reactn"> React Native</label>
+          <br />
+          <input type="checkbox" name="redux" checked readOnly />
+          <label htmlFor="redux"> Redux</label>
+          <br />
+          <input type="checkbox" name="angular" checked="" readOnly />
+          <label htmlFor="angular"> Angular</label>
+          <br />
+          <p>Version Control</p>
+          <input type="checkbox" name="git" checked readOnly />
+          <label htmlFor="git"> Git/GitHub</label>
+          <br />
+          <input type="checkbox" name="AWS" checked readOnly />
+          <label htmlFor="AWS"> AWS CodeCommit</label>
+          <br />
+          <p>Testing Frameworks</p>
+          <input type="checkbox" name="postman" checked readOnly />
+          <label htmlFor="postman"> Postman</label>
+          <br />
+          <input type="checkbox" name="selenium" checked="" readOnly />
+          <label htmlFor="selenium"> Selenium</label>
+          <br />
+          <input type="checkbox" name="jest" checked="" readOnly />
+          <label htmlFor="jest"> Jest</label>
+          <br />
+          <input type="checkbox" name="cypress" checked="" readOnly />
+          <label htmlFor="cypress"> Cypress</label>
+          <br />
+          <p>Database Programs</p>
+          <input type="checkbox" name="sql" checked="" readOnly />
+          <label htmlFor="sql"> SQL</label>
+          <br />
+          <input type="checkbox" name="mongo" checked="" readOnly />
+          <label htmlFor="mongo"> MongoDB</label>
+          <br />
+        </div>
+      </Goals>
+    </Container>
+  )
 }
 
 const Container = styled(motion.div)`
-display: flex;
-@media only screen and (max-width: 1200px) {
-        flex-direction: column;
-    }
-.milestones {
+  display: flex;
+  @media only screen and (max-width: 1200px) {
+    flex-direction: column;
+  }
+  .milestones {
     display: flex;
     flex-direction: column;
     align-items: center;
     width: 50vw;
     h3 {
-        text-align: center;
-        margin-bottom: 3rem;
+      text-align: center;
+      margin-bottom: 3rem;
     }
     @media only screen and (max-width: 1200px) {
-       width: 100%;
-       margin-bottom: 2rem;
+      width: 100%;
+      margin-bottom: 2rem;
     }
-}
-
+  }
 `
 
 const Goals = styled(motion.div)`
-width: 50vw;
-padding-bottom: 1rem;
-h3 {
+  width: 50vw;
+  padding-bottom: 1rem;
+  h3 {
     margin-bottom: 0;
-}
-i {
+  }
+  i {
     font-size: 10px;
-}
-p {
+  }
+  p {
     font-weight: 700;
-}
-@media only screen and (max-width: 1200px) {
-       width: 100%;
-       .text {
-           padding-left: 2rem;
-       }
+  }
+  @media only screen and (max-width: 1200px) {
+    width: 100%;
+    .text {
+      padding-left: 2rem;
     }
+  }
 `
 
-export default Milestones;
+export default Milestones
--- src/timelinedata.js
+++ src/timelinedata.js
@@ -1,35 +1,36 @@
 const timelineData = [
-    {
-        text: "Added a new project to my portfolio, studying for the ISTQB exam",
-        date: "March 2021",
-    },{
-        text: "Working on the Harvard CS50x course",
-        date: "February 2021",
-    },
-    {
-        text: "Completed the JavaScript30 course (One fun JavaScript project for each day)",
-        date: "January 2021",
-    },
-    {
-        text: "Building out my Portfolio Website",
-        date: "December 2020",
-    },
-    {
-        text: "Working with ReactJS, completion of The Creative React and Redux Course by Dev Ed",
-        date: "November 2020",
-    },
-    {
-        text: "First applications completed (Quiz App, Weather App)",
-        date: "October 2020",
-    },
-    {
-        text: "HTML/CSS/JavaScript tutorials, Javascript coding exercises on w3resource.com",
-        date: "September 2020",
-    },
-    {
-        text: "Wrote my first Hello World in HTML and fell in love with web development",
-        date: "August 20, 2020",
-    }
-];
+  {
+    text: "Passed the ISTQB CTFL exam",
+    date: "May 2021",
+  },
+  {
+    text: "Started to study the Harvard CS50x course",
+    date: "February 2021",
+  },
+  {
+    text: "Completed the JavaScript30 course (One fun JavaScript project for each day)",
+    date: "January 2021",
+  },
+  {
+    text: "Building out my Portfolio Website",
+    date: "December 2020",
+  },
+  {
+    text: "Working with ReactJS, completion of The Creative React and Redux Course by Dev Ed",
+    date: "November 2020",
+  },
+  {
+    text: "First applications completed (Quiz App, Weather App)",
+    date: "October 2020",
+  },
+  {
+    text: "HTML/CSS/JavaScript tutorials, Javascript coding exercises on w3resource.com",
+    date: "September 2020",
+  },
+  {
+    text: "Wrote my first Hello World in HTML and fell in love with web development",
+    date: "August 20, 2020",
+  },
+]
 
-export default timelineData;
+export default timelineData
